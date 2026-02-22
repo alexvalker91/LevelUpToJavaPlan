2dcf7846a3e579302e9248fbbbaa65eb9ab837c9
Aliaksandr_Kreyer <Aliaksandr_Kreyer@epam.com>
2026-02-22 14:05:31 +0300

    Add Spring MVC

---
+++ Java Global Learning Journey/Spring MVC/SpringMVC/src/test/java/alex/valker91/SpringMVC/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.valker91.SpringMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
