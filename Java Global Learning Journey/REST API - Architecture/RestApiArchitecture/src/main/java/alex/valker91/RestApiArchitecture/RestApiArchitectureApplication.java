bf2981c9cadfabefe3a0541532896645d17e625e
Aliaksandr_Kreyer <Aliaksandr_Kreyer@epam.com>
2026-02-25 13:08:36 +0300

    Add RestApiArchitecture project

---
+++ Java Global Learning Journey/REST API - Architecture/RestApiArchitecture/src/main/java/alex/valker91/RestApiArchitecture/RestApiArchitectureApplication.java
@@ -0,0 +1,13 @@
+package alex.valker91.RestApiArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiArchitectureApplication.class, args);
+	}
+
+}
---
+++ Java Global Learning Journey/REST API - Architecture/RestApiArchitecture/src/test/java/alex/valker91/RestApiArchitecture/RestApiArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.valker91.RestApiArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
