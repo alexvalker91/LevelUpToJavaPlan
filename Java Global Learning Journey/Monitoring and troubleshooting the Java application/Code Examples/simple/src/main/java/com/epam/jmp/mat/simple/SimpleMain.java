1b10998fee6695f1dffb901a7a4c6969ba593aca
Aliaksandr_Kreyer <Aliaksandr_Kreyer@epam.com>
2026-02-26 15:44:52 +0300

    Remove

--- Java Global Learning Journey/Monitoring and troubleshooting the Java application/Code Examples/simple/src/main/java/com/epam/jmp/mat/simple/SimpleMain.java
+++
@@ -1,9 +0,0 @@
-package com.epam.jmp.mat.simple;
-
-public class SimpleMain {
-
-    public static void main(String[] args) throws java.io.IOException {
-        System.out.println("Press any key to proceed");
-        System.in.read();
-    }
-}
